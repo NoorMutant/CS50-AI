5197fa840e6df14704683fbf885533cfc6fd8ee3
NoorMutant <141281308+NoorMutant@users.noreply.github.com>
2024-08-15 03:59:53 +0500

    all done

--- week 0/TIC TAC TOE/tictactoe/tictactoe.py
+++ week 0/TIC TAC TOE/tictactoe/tictactoe.py
@@ -114,18 +114,63 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
-
+    if winner(board) == X:
+        return True
+    if winner(board) == O:
+        return True
+    """if game is remaining"""
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            if board[row][col] == EMPTY:
+                return False
+    """if game is tie"""
+    return True
+            
+    
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
+
+def max_value(board):
+    v= -math.inf
+    if terminal(board):
+        return utility(board)
+    for action in actions(board):
+        v = max(v, min_value(result(board,action)))
+        return v
+    
+
+def min_value(board):
+    v= math.inf
+    if terminal(board):
+        return utility(board)
+    for action in actions(board):
+        v = min(v, max_value(result(board,action)))
+        return v
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+    
+    elif player(board) == X:
+        plays = []
+        for action in actions(board):
+            plays.append([min_value(result(board,action)),action])
+        return sorted(plays, key=lambda x: x[0], reverse=True)[0][1]
+    elif player(board) == O:
+        plays = []
+        for action in actions(board):
+            plays.append([max_value(result(board,action)),action])
+        return sorted(plays, key=lambda x: x[0])[0][1]
