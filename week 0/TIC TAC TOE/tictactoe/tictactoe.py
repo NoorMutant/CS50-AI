a783068a9a82768a9aca16a26fe05c72d987b97a
NoorMutant <141281308+NoorMutant@users.noreply.github.com>
2024-08-16 04:08:42 +0500

    week 0 full done

--- week 0/TIC TAC TOE/tictactoe/tictactoe.py
+++ week 0/TIC TAC TOE/tictactoe/tictactoe.py
@@ -14,9 +14,7 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+    return [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
 
 
 def player(board):
@@ -30,13 +28,12 @@ def player(board):
         for col in range(len(board[row])):
             if board[row][col] == X:
                 countx += 1
-            if board[row][col]  == O:
+            if board[row][col] == O:
                 counto += 1
-    if countx > counto :
+    if countx > counto:
         return O
     else:
         return X
-
 
 
 def actions(board):
@@ -47,9 +44,10 @@ def actions(board):
     for row in range(len(board)):
         for col in range(len(board[row])):
             if board[row][col] == EMPTY:
-                allPossibleActions.add((row,col))
-    
+                allPossibleActions.add((row, col))
+
     return allPossibleActions
+
 
 def result(board, action):
     """
@@ -57,25 +55,31 @@ def result(board, action):
     """
     if action not in actions(board):
         raise Exception("Not Valid Action")
-    
+
     row, col = action
     board_copy = copy.deepcopy(board)
     board_copy[row][col] = player(board)
     return board_copy
 
 
-
-
 def checkRow(board, current_player):
     for row in range(len(board)):
-        if board[row][0] == current_player and board[row][1] == current_player and board[row][2] == current_player:
+        if (
+            board[row][0] == current_player
+            and board[row][1] == current_player
+            and board[row][2] == current_player
+        ):
             return True
     return False
 
 
 def checkCol(board, current_player):
     for col in range(len(board)):
-        if board[0][col] == current_player and board[1][col] == current_player and board[2][col] == current_player:
+        if (
+            board[0][col] == current_player
+            and board[1][col] == current_player
+            and board[2][col] == current_player
+        ):
             return True
     return False
 
@@ -101,16 +105,28 @@ def checkSecondDiagonal(board, current_player):
     else:
         return False
 
+
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    if checkRow(board, X) or checkCol(board, X) or checkFirstDiagonal(board, X) or checkSecondDiagonal(board, X):
+    if (
+        checkRow(board, X)
+        or checkCol(board, X)
+        or checkFirstDiagonal(board, X)
+        or checkSecondDiagonal(board, X)
+    ):
         return X
-    elif checkRow(board, O) or checkCol(board, O) or checkFirstDiagonal(board, O) or checkSecondDiagonal(board, O):
+    elif (
+        checkRow(board, O)
+        or checkCol(board, O)
+        or checkFirstDiagonal(board, O)
+        or checkSecondDiagonal(board, O)
+    ):
         return O
     else:
         return None
+
 
 def terminal(board):
     """
@@ -127,8 +143,7 @@ def terminal(board):
                 return False
     """if game is tie"""
     return True
-            
-    
+
 
 def utility(board):
     """
@@ -143,21 +158,22 @@ def utility(board):
 
 
 def max_value(board):
-    v= -math.inf
+    v = -math.inf
     if terminal(board):
         return utility(board)
     for action in actions(board):
-        v = max(v, min_value(result(board,action)))
+        v = max(v, min_value(result(board, action)))
     return v
-    
+
 
 def min_value(board):
-    v= math.inf
+    v = math.inf
     if terminal(board):
         return utility(board)
     for action in actions(board):
-        v = min(v, max_value(result(board,action)))
+        v = min(v, max_value(result(board, action)))
     return v
+
 
 def minimax(board):
     """
@@ -165,14 +181,14 @@ def minimax(board):
     """
     if terminal(board):
         return None
-    
+
     elif player(board) == X:
         plays = []
         for action in actions(board):
-            plays.append([min_value(result(board,action)),action])
+            plays.append([min_value(result(board, action)), action])
         return sorted(plays, key=lambda x: x[0], reverse=True)[0][1]
     elif player(board) == O:
         plays = []
         for action in actions(board):
-            plays.append([max_value(result(board,action)),action])
+            plays.append([max_value(result(board, action)), action])
         return sorted(plays, key=lambda x: x[0])[0][1]
